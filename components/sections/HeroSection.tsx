87f07eb3aaae70c8e7c28ae431ee5ab84eb1e4dd
Ansh Tank <137699260+AnshTank@users.noreply.github.com>
2025-11-09 15:02:00 +0530

    Update URL in HeroSection component

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
                 className="group px-6 sm:px-8 py-4 sm:py-6 text-base sm:text-lg font-medium shadow-lg hover:shadow-xl transition-all duration-300 w-full sm:w-auto"
                 onClick={() =>
                   window.open(
-                    "https://rendercv-sb2nov-theme-3.tiiny.site",
+                    "https://pink-kimberlyn-57.tiiny.site/",
                     "_blank"
                   )
                 }
